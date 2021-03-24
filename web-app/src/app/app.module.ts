923bf95b19ecd446a982e3bfc61c9106fb98d167
James Schuler <jameshschuler1@gmail.com>
2021-03-23 22:34:18 -0400

    initial create/join room forms

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomFormComponent } from './components/create-room-form/create-room-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JoinRoomComponent } from './components/join-room/join-room.component';
 
-@NgModule({
+
+@NgModule( {
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomFormComponent,
+    NavbarComponent,
+    JoinRoomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [ AppComponent ]
+} )
 export class AppModule { }
---
+++ web-app/src/app/components/create-room-form/create-room-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component( {
+  selector: 'create-room-form',
+  templateUrl: './create-room-form.component.html',
+  styleUrls: [ './create-room-form.component.scss' ]
+} )
+export class CreateRoomFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit (): void {
+  }
+
+}
